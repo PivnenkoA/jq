aba0eba03dee19f33ba0cd7805585b43dc8533cb
runemaker <runesmaker@yandex.ru>
2018-11-05 18:06:49 +0300

    Проба пера

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
-	
+	$('#kah').on('click',function(){
+		document.getElementById('textContainer').innerHTML = 'Hello! I am a robot. I am here to kill all humans!</br><a href="index.html">CLEAR</a>'	
+	})
 	
 })
